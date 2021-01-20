bea245909199af234e364af8ac12ebf06cd74c18
zhangwenzhen <zhangwenzhen@chinamobile.com>
2021-01-20 16:25:36 +0800

    add to github

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    // 是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
+    @Value(value = "${swagger.enabled}")
+    Boolean swaggerEnabled;
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                // 是否开启
+                .enable(swaggerEnabled).select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("接口api") //文档标题
+                .description("api公共资源代理接口")//接口概述
+                .version("1.0") //版本号
+                .termsOfServiceUrl(String.format("url"))//服务的域名
+                .build();
+    }
+}
